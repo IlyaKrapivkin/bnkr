a04291f921d2cafe1b14dd654f95a55dfe496e26
ilyaluber <iliakrapivkin@gmail.com>
2023-03-26 22:41:31 +0300

    linter installed

--- run.js
+++ run.js
@@ -1,48 +1,50 @@
-const http = require('http');
-const url = require('url');
+const http = require(`http`);
+const url = require(`url`);
 
-const localhost = `127.0.0.1 `;
-const port = 3001;
-const methodGet = `GET`;
-const methodPost = `POST`;
-const statusOk = 200;
-const statusIncorrect = 404;
-const messageStart = `🚀 server started`;
-const messageOk = `OK`;
-const messageIncorrect = `INCORRECT`;
-const contentType = `text/plain`;
+// const str_localhost = `127.0.0.1 `;
+
+const num_port = 3001;
+const num_statusOk = 200;
+const num_statusIncorrect = 404;
+const str_methodGet = `GET`;
+const str_methodPost = `POST`;
+const str_messageStart = `🚀 server started on port [${ num_port }]`;
+const str_messageOk = `OK`;
+const str_messageIncorrect = `INCORRECT`;
+const str_contentTypeText = `text/plain`;
 
 const server = http.createServer((req, res) => {
+  const urlObj = url.parse(req.url, true);
+  console.log(urlObj);
+
   switch (req.method) {
-    case methodGet:
-      console.log(methodGet);
-      const urlObj = url.parse(req.url, true);
-      console.log(urlObj);
+    case str_methodGet:
+      console.log(str_methodGet);
       res.writeHead(
-        statusOk,
-        { 'Content-Type': contentType },
+        num_statusOk,
+        { 'Content-Type': str_contentTypeText },
       );
-      res.end(`${methodGet} ${messageOk}`);
+      res.end(`${ str_methodGet } ${ str_messageOk }`);
     break;
 
-    case methodPost:
-      console.log(methodPost);
+    case str_methodPost:
+      console.log(str_methodPost);
       res.writeHead(
-        statusOk,
-        { 'Content-Type': contentType },
+        num_statusOk,
+        { 'Content-Type': str_contentTypeText },
       );
-      res.end(`${methodPost} ${messageOk}`);
+      res.end(`${ str_methodPost } ${ str_messageOk }`);
     break;
 
     default:
       console.log(req.method);
       res.writeHead(
-        statusIncorrect,
-        { 'Content-Type': contentType },
+        num_statusIncorrect,
+        { 'Content-Type': str_contentTypeText },
       );
-      res.end(messageIncorrect);
+      res.end(str_messageIncorrect);
     break;
   }
 });
 
-server.listen(port, () => console.log(messageStart));
+server.listen(num_port, () => console.log(str_messageStart));
