8bcc8ac6c2dfc4f08610eb74012479cbeba94aac
ilyaluber <iliakrapivkin@gmail.com>
2023-03-26 23:46:58 +0300

    connection check

--- run.js
+++ run.js
@@ -1,5 +1,5 @@
 const http = require(`http`);
-const url = require(`url`);
+// const url = require(`url`);
 
 // const str_localhost = `127.0.0.1 `;
 
@@ -8,14 +8,22 @@ const num_statusOk = 200;
 const num_statusIncorrect = 404;
 const str_methodGet = `GET`;
 const str_methodPost = `POST`;
-const str_messageStart = `🚀 server started on port [${ num_port }]`;
+const str_messageStart = `🚀 server started on port [${ num_port }]\n`;
+const str_messageConnection = `🚂 new connection`;
+const str_messageRequest = `🚗 new request`;
 const str_messageOk = `OK`;
 const str_messageIncorrect = `INCORRECT`;
 const str_contentTypeText = `text/plain`;
 
-const server = http.createServer((req, res) => {
-  const urlObj = url.parse(req.url, true);
-  console.log(urlObj);
+const server = http.createServer();
+
+server.on(`connection`, connection => {
+  console.log(str_messageConnection)
+})
+
+server.on(`request`, (req, res) => {
+  // const urlObj = url.parse(req.url, true);
+  console.log(str_messageRequest);
 
   switch (req.method) {
     case str_methodGet:
