1ee7525dbcd61720e86fc2e947ec8ba5057bc8dd
ilyaluber <iliakrapivkin@gmail.com>
2023-04-30 18:44:30 +0300

    methods and routes rewritten

--- run.js
+++ run.js
@@ -1,50 +1,88 @@
 const http = require(`http`);
+const config = require(`./package.json`);
 
 const num_port = 3001;
 const num_statusOk = 200;
-const num_statusIncorrect = 404;
+const num_statusUnauthorized = 401;
+const num_statusNotFound = 404;
+const num_methodNotAllowed = 405;
 const str_host = `127.0.0.1`;
 const str_methodGet = `GET`;
 const str_methodPost = `POST`;
-const str_messageOk = `OK`;
-const str_messageIncorrect = `INCORRECT`;
 const str_messageStart = `🚀 server started on port [${ num_port }]\n`;
-const str_messageRequest = `🚗 new request`;
-const str_contentType = `Content-Type`;
+const str_iconRequest = `🚗`;
+const str_headerContentType = `Content-Type`;
+const str_headerAuthorization = `authorization`;
 const str_contentTypeText = `text/plain`;
+const str_routePing = `/ping`;
+const str_routeInfoApp = `/infoApp`;
+const str_pong = `PONG`;
+const str_notFound = `NOT FOUND`;
+const str_unauthorized = `UNAUTHORIZED`;
+const str_methodNotAllowed = `METHOD NOT ALLOWED`;
 
 const fun_listeningListener = () => {
   console.log(str_messageStart)
 }
 
-const fun_requestListener = (incomingMessage, serverResponse) => {
-  console.log(str_messageRequest);
+const server = http.createServer((incomingMessage, serverResponse) => {
+  console.log(`${str_iconRequest} [${incomingMessage.method}] [${incomingMessage.url}]`);
+
+  const any_sessionInitial = incomingMessage.headers[str_headerAuthorization];
+  const str_session = (
+    (
+      any_sessionInitial &&
+      typeof any_sessionInitial === `string` &&
+      any_sessionInitial.length
+    ) ?
+    any_sessionInitial :
+    ``
+  );
+  const bol_authorized = (str_session === `12345`);
 
   switch (incomingMessage.method) {
     case str_methodGet:
-      console.log(str_methodGet);
-      serverResponse.setHeader(str_contentType, str_contentTypeText);
-      serverResponse.statusCode = num_statusOk;
-      serverResponse.end(`${ str_methodGet } ${ str_messageOk }`);
+      switch (incomingMessage.url) {
+        case str_routePing:
+          serverResponse.setHeader(str_headerContentType, str_contentTypeText);
+          serverResponse.statusCode = num_statusOk;
+          serverResponse.end(str_pong);
+        break;
+
+        case str_routeInfoApp:
+          if (bol_authorized) {
+            serverResponse.setHeader(str_headerContentType, str_contentTypeText);
+            serverResponse.statusCode = num_statusOk;
+            const str_infoApp = `${config.name} ${config.version} ${config.description}`;
+            serverResponse.end(str_infoApp);
+          } else {
+            serverResponse.setHeader(str_headerContentType, str_contentTypeText);
+            serverResponse.statusCode = num_statusUnauthorized;
+            serverResponse.end(str_unauthorized);
+          }
+        break;
+      
+        default:
+          serverResponse.setHeader(str_headerContentType, str_contentTypeText);
+          serverResponse.statusCode = num_statusNotFound;
+          serverResponse.end(str_notFound);
+        break;
+      }
     break;
 
     case str_methodPost:
-      console.log(str_methodPost);
-      serverResponse.setHeader(str_contentType, str_contentTypeText);
-      serverResponse.statusCode = num_statusOk;
-      serverResponse.end(`${ str_methodPost } ${ str_messageOk }`);
+      serverResponse.setHeader(str_headerContentType, str_contentTypeText);
+      serverResponse.statusCode = num_statusNotFound;
+      serverResponse.end(str_notFound);
     break;
 
     default:
-      console.log(incomingMessage.method);
-      serverResponse.setHeader(str_contentType, str_contentTypeText);
-      serverResponse.statusCode = num_statusIncorrect;
-      serverResponse.end(str_messageIncorrect);
+      serverResponse.setHeader(str_headerContentType, str_contentTypeText);
+      serverResponse.statusCode = num_methodNotAllowed;
+      serverResponse.end(str_methodNotAllowed);
     break;
   }
-}
-
-const server = http.createServer(fun_requestListener);
+})
 
 server.listen(
   num_port,
